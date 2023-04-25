71b49c64216912b674a4773913553de851d07116
belen luna <barbarabluna8@gmail.com>
2023-04-25 15:02:25 -0300

    fondo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Imagen from "./img/logo.png";
+import Video from "./img/video.mp4";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <nav className="nav">
+        <img className="img1" src={Imagen} />
+        <ul>
+          <li>
+            <a href="Accomodations">Alojamientos</a>
+          </li>
+          <li>
+            <a href="Contac">Contacto</a>
+          </li>
+          <li>
+            <a href="Blog">Blog</a>
+          </li>
+        </ul>
+      </nav>
+      <video className="video" autoPlay loop muted>
+        <source src={Video} type="video/mp4" />
+      </video>
+    </div>
+  );
 };
 
 export default NavBar;
